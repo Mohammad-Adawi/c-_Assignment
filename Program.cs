192efcb0e9058989d8b44ed16288e39a1eae877e
Mohammad <adawi83@hotmail.com>
2020-05-08 17:07:59 -0600

    split

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.IO;
 
 namespace C__weekend_Assignment
@@ -9,13 +8,30 @@ namespace C__weekend_Assignment
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Prompt: Please Enter File Name:"); 
+            Console.WriteLine("Prompt: Please Enter a File Name:");
             string fileName = Console.ReadLine();
-            File.WriteAllText(fileName, "you are inside" + " " + fileName ); 
+            File.WriteAllText(fileName, "this is your new file");
             Console.WriteLine("your file name: " + fileName);
 
-           
-          
+
+            Console.WriteLine("Enter a string:");
+            string myString = Console.ReadLine();
+
+            string[] myStringArray = myString.Split(" ");
+            foreach (string element in myStringArray)
+            {
+                File.WriteAllText(fileName, element);
+                Console.WriteLine(element);
+
+            }
+
+            int option = 1;
+            do {
+              option++;
+            } 
+
+            while (option <= 3);      
+            Console.WriteLine("Terminated"); 
              
         }
     }
