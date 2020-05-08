620405cd9cf4b69881436a80150be625c5debda6
Mohammad <adawi83@hotmail.com>
2020-05-08 14:34:58 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+
+namespace C__weekend_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Prompt: Please Enter File Name:"); 
+            string fileName = Console.ReadLine();
+            File.WriteAllText(fileName, "you are inside" + " " + fileName ); 
+            Console.WriteLine("your file name: " + fileName);
+
+           
+          
+             
+        }
+    }
+}
